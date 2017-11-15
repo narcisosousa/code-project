684df13b3943cca039d4f36ee98fe5e5f737f87f
Narciso <narciso-ps@hotmail.com>
2017-11-15 18:41:24 -0300

    fase 5

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,16 @@ config.vendor_path_css = [
     config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css'
 ];
 
+config.build_path_html = config.build_path+'/views';
+
+gulp.task('copy-html', function () {
+    gulp.src([
+        config.assets_path + '/js/views/**/*.html'
+    ])
+        .pipe(gulp.dest(config.build_path_html))
+        .pipe(liveReload());
+});
+
 gulp.task('copy-styles', function () {
     gulp.src([
         config.assets_path + '/css/**/*.css'
@@ -54,6 +64,7 @@ gulp.task('copy-scripts', function () {
 });
 
 gulp.task('default', ['clear-build-folder'],function () {
+    gulp.start('copy-html');
     elixir(function (mix) {
         mix.styles(config.vendor_path_css.concat([config.assets_path+'/css/**/*.css']), 'public/css/all.css', config.assets_path);
         mix.styles(config.vendor_path_js.concat([config.assets_path+'/js/**/*.js']), 'public/js/all.js', config.assets_path);
@@ -63,8 +74,8 @@ gulp.task('default', ['clear-build-folder'],function () {
 
 gulp.task('watch-dev', ['clear-build-folder'], function () {
     liveReload.listen();
-    gulp.start('copy-styles', 'copy-scripts');
-    gulp.watch(config.assetsPath + "/**", ['copy-styles', 'copy-scripts']);
+    gulp.start('copy-styles', 'copy-scripts', 'copy-html');
+    gulp.watch(config.assets_path + "/**", ['copy-styles', 'copy-scripts', 'copy-html']);
 });
 
 gulp.task('clear-build-folder', function () {
